a94b81593fc9a50a725ce1b2b1afbdaa8f06665e
WindyJasmin <123154941+WindyJasmin@users.noreply.github.com>
2023-12-19 22:37:34 +0100

    Updated the Chat interface to test the message sending process + updating the SignalR service + app.jsx to call the ChatInterface

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,13 @@
 // App.jsx
-import axios from "axios";
-import  { useState } from "react";
-import SignUpForm from "./components/SignUpForm";
+
+import ChatInterface from "./components/ChatInterface";
 import "./App.css"; // Import a separate CSS file for styling
 
 function App() {
-  const [form, setForm] = useState({});
-  const handleChange = (event) => {
-    setForm({ ...form, [event.target.name]: event.target.value });
-  };
-
-  const onSubmit = async (event) => {
-    event.preventDefault();
-
-    const formData = new FormData();
-    formData.append("Email", form.Email);
-    formData.append("Password", form.Password);
-    formData.append("ProfilePic", form.ProfilePic);
-
-    console.log("aaaa");
-    await axios.post("https://localhost:7225/api/Auth/register", formData, {
-      headers: {
-        "Content-Type": "multipart/form-data",
-      },
-    });
-  };
 
   return (
     <div id ="root" >
-      <SignUpForm
-        form={form}
-        handleChange={handleChange}
-        onSubmit={onSubmit}
-      />
+       <ChatInterface />
     </div>
   );
 }
--- src/components/ChatInterface.jsx
+++ src/components/ChatInterface.jsx
@@ -1,97 +1,60 @@
-// ChatInterface.js
-
-import { useState, useEffect } from 'react';
-import signalRService from './signalRService'; // Import your SignalR service
+import  { useState, useEffect } from 'react';
+import { startConnection, addMessageListener, sendMessage, stopConnection } from '../services/signalrService';
 
 const ChatInterface = () => {
   const [message, setMessage] = useState('');
   const [messages, setMessages] = useState([]);
-  // we will use this later
-  //const [connectedUsers, setConnectedUsers] = useState([]);
- // const [activeChats, setActiveChats] = useState([]);
 
   useEffect(() => {
-    // Start SignalR connection when the component mounts
-    signalRService.startConnection();
+    // Initialize SignalR connection
+    startConnection();
 
-    // Add a listener for incoming messages
-    signalRService.addMessageListener((messageData) => {
+    // Add message listener to update state with incoming messages
+    addMessageListener((messageData) => {
       setMessages((prevMessages) => [...prevMessages, messageData]);
     });
 
-    // Cleanup SignalR connection when the component unmounts
+    // Clean up SignalR connection on component unmount
     return () => {
-      signalRService.stopConnection();
+      stopConnection();
     };
   }, []);
 
-  const sendMessage = () => {
-    // Implement your logic to send messages
-    // You may want to call a function in your SignalR service to send the message
-    // signalRService.sendMessage(receiverUserId, message);
+  const handleSendMessage = async () => {
+    try {
+      if (message.trim() !== '') {
+        const user = 'user';
+        await sendMessage(user, message);
+        setMessage('');
+      }
+    } catch (error) {
+      console.error('Error sending message:', error);
+    }
   };
 
   return (
-    <div className="flex h-screen bg-gray-100">
-      {/* Sidebar with user list */}
-      <div className="w-1/4 p-4 bg-gray-200">
-        <h2 className="text-xl font-semibold mb-4">Connected Users</h2>
-        <ul>
-          
-          {
-          /* This is the list for the connected users */
-          /*connectedUsers.map((user) => (
-            <li key={user.id}>{user.name}</li>
-          )) */}
-        </ul>
+    <div className="flex flex-col h-screen items-center justify-center">
+      {/* Display the chat messages */}
+      <div className="overflow-y-auto p-4 max-h-96 border border-gray-300 w-96">
+        {messages.map((msg, index) => (
+          <div key={index} className="my-2">
+            <strong>{msg.user}:</strong> {msg.message}
+          </div>
+        ))}
       </div>
 
-      {/* Main chat area */}
-      <div className="flex-1 flex flex-col">
-        {/* Connected users at the top */}
-        <div className="p-4 bg-gray-300">
-          <h2 className="text-xl font-semibold">Connected Users</h2>
-          <ul className="flex">
-             { /* This is the list for the connected users *
-             connectedUsers.map((user) => (
-              <li key={user.id} className="mr-2">
-                {user.name}
-              </li>
-             ))*/}
-          </ul>
-        </div>
-
-        {/* Active chat list */}
-        <div className="flex-1 overflow-y-auto p-4">
-          {/*activeChats.map((chat) => (
-            <div key={chat.userId} className="mb-4">
-              <h3 className="text-lg font-semibold">{chat.userName}</h3>
-              <div className="flex flex-col">
-                {messages
-                  .filter((msg) => msg.user === chat.userId)
-                  .map((msg, index) => (
-                    <div key={index} className="my-2">
-                      {msg.message}
-                    </div>
-                  ))}
-              </div>
-            </div>
-                  ))*/}
-        </div>
-
-        {/* Message input and send button */}
-        <div className="p-4 bg-gray-300">
-          <input
-            type="text"
-            value={message}
-            onChange={(e) => setMessage(e.target.value)}
-            placeholder="Type your message..."
-            className="border p-2 w-full"
-          />
-          <button onClick={sendMessage} className="mt-2 bg-blue-500 text-white p-2 rounded">
-            Send
-          </button>
-        </div>
+      {/* Message input and send button */}
+      <div className="p-4 bg-gray-300 w-96">
+        <input
+          type="text"
+          value={message}
+          onChange={(e) => setMessage(e.target.value)}
+          placeholder="Type your message..."
+          className="border p-2 w-full"
+        />
+        <button onClick={handleSendMessage} className="mt-2 bg-blue-500 text-white p-2 rounded">
+          Send
+        </button>
       </div>
     </div>
   );
