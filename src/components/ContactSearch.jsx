169191d57119228ace7296c965d9caf144c80c51
Zeineb Ben Abdallah <zeineb.benabdallah@insat.ucar.tn>
2024-02-06 21:57:10 +0100

    debounce works

--- src/components/ContactSearch.jsx
+++ src/components/ContactSearch.jsx
@@ -8,114 +8,63 @@ import SearchIcon from "@mui/icons-material/Search";
 import FilterListRoundedIcon from "@mui/icons-material/FilterListRounded";
 import "../styles/ContactSearch.css";
 import axiosInstance from "../utils/axiosInstance";
-import { useState, useEffect, useRef } from "react";
+import React, { useState, useEffect, useRef, useCallback } from "react";
+import {
+  debounceTime,
+  switchMap,
+  map,
+  scan,
+  tap,
+  throttleTime,
+  distinctUntilChanged,
+} from "rxjs/operators";
+import { Subject, fromEvent, merge, from } from "rxjs";
+import { useInView } from "react-intersection-observer";
 import Avatar from "@mui/material/Avatar";
 import Badge from "@mui/material/Badge";
 import PropTypes from "prop-types";
-import useDebounce from "../customHooks/useDebounce";
-import InfiniteScroll from "react-infinite-scroller";
+
 const backend_url = import.meta.env.VITE_BackendURL;
+const fetchSearchUsers = async (query, page) => {
+  // Fetch contacts based on the query and page
+  try {
+    const res = await axiosInstance.get(`/users/search/${query}/${page}`);
+    return res.data;
+  } catch (err) {
+    console.error(err);
+  }
+};
 
 const ContactSearch = ({ onContactSelect }) => {
   // const [searchTerm, setSearchTerm] = useState("");
   // const [contacts, setContacts] = useState([]);
-
   const [selectedContact, setSelectedContact] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-  const [error, setError] = useState(null);
-  const [hasMore, setHasMore] = useState(true);
-  const [page, setPage] = useState(1);
-  const observerTarget = useRef(null);
-
-  useEffect(() => {
-    console.log("aaaa");
-    const observer = new IntersectionObserver(
-      (entries) => {
-        if (entries[0].isIntersecting) {
-          fetchMoreSearchData();
-        }
-      },
-      { threshold: 1 }
-    );
-
-    if (observerTarget.current) {
-      observer.observe(observerTarget.current);
-    }
-
-    return () => {
-      console.log("aaa");
-      if (observerTarget.current) {
-        observer.unobserve(observerTarget.current);
-      }
-    };
-  }, [observerTarget]);
-
-  // useEffect(() => {
-  //   axiosInstance
-  //     .get("/users")
-  //     .then((response) => {
-  //       console.log(response.data);
-  //       setContacts(response.data);
-  //     })
-  //     .catch((error) => {
-  //       console.error("Error fetching users:", error);
-  //     });
-  // }, []);
-
-  // const handleSearchChange = (event) => {
-  //   setSearchTerm(event.target.value);
-  // };
-
   const handleCardClick = (contactId) => {
     setSelectedContact(contactId === selectedContact ? null : contactId);
     onContactSelect(contactId);
   };
-
-  // const filteredContacts = contacts.filter((contact) =>
-  //   contact.userName.toLowerCase().startsWith(searchTerm.toLowerCase())
-  // );
-
-  // start here working logic, all of the above code has to be reviewed
   const [query, setQuery] = useState("");
-  const queryRef = useRef(query);
-  const [searchUsers, setSearchUsers] = useState([]);
+  const [page, setPage] = useState(1);
+  const [contacts, setContacts] = useState([]);
+  const inputRef = useRef(null);
+  useEffect(() => {
+    const query$ = fromEvent(inputRef.current, "input").pipe(
+      debounceTime(1000),
+      distinctUntilChanged(),
+      switchMap((e) => fetchSearchUsers(e.target.value, 1))
+    );
 
-  const fetchMoreSearchData = () => {
-    console.log(query);
-    if (!hasMore) return;
-    axiosInstance
-      .get(`/users/search/${query}/${page}`)
-      .then((response) => {
-        console.log(response.data);
-        if (response.data.length) setPage((prev) => prev + 1);
-        else setHasMore(false);
-        setSearchUsers((prev) => [...prev, ...response.data]);
-      })
-      .catch((error) => {
-        console.error("Error fetching users:", error);
-      });
-  };
+    const sub = query$.subscribe({
+      next: (res) => {
+        console.log(res);
+        setContacts(res);
+      },
+      error: (err) => console.error(err),
+    });
+    return () => sub.unsubscribe();
+  }, []);
 
-  const debouncedRequest = useDebounce(() => {
-    setPage(0);
-    setHasMore(true);
-    if (query.trim().length == 0) {
-      setSearchUsers([]);
-      return;
-    }
-    //search backend request here
-    axiosInstance
-      .get(`/users/search/${query}/1`)
-      .then((response) => {
-        console.log(response.data);
-        setSearchUsers(response.data);
-      })
-      .catch((error) => {
-        console.error("Error fetching users:", error);
-      });
-    console.log(query);
-  });
-  const displayedUsers = searchUsers.map((contact) => (
+  const displayedUsers = contacts.map((contact) => (
     <div
       key={contact.id}
       style={{
@@ -159,12 +108,6 @@ const ContactSearch = ({ onContactSelect }) => {
       </div>
     </div>
   ));
-  const onChange = (e) => {
-    const value = e.target.value;
-    setQuery(value);
-    debouncedRequest();
-  };
-
   return (
     <div className="contacts-list max-h-full overflow-auto">
       <h2
@@ -201,7 +144,10 @@ const ContactSearch = ({ onContactSelect }) => {
           sx={{ ml: 1, flex: 1 }}
           placeholder="Search"
           inputProps={{ "aria-label": "search" }}
-          onChange={onChange}
+          onChange={(e) => setQuery(e.target.value)}
+          value={query}
+          name="query"
+          ref={inputRef}
         />
         <IconButton
           color="primary"
@@ -221,9 +167,6 @@ const ContactSearch = ({ onContactSelect }) => {
         className=" overflow-auto max-h-full"
       >
         {displayedUsers}
-        {isLoading && <p>Loading...</p>}
-        {error && <p>Error: {error.message}</p>}
-        <div ref={observerTarget}></div>
       </div>
     </div>
   );
